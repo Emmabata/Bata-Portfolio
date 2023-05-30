849088bb6e886567f69607aa49078171b416f7b5
Emmabata <ndinezeemmanuel@gmail.com>
2023-05-30 17:30:10 +0100

    first push

---
+++ react-portfolio/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
+
+      <main></main>
+    </div>
+  )
+}
